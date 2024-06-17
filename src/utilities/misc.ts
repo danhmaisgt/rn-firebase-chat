acd78d21c651e6dcccf0d8825d3b1670f7b4dbba
HuyDo <huy.do@gmail.com>
2024-06-17 15:50:43 +0700

    feat: [FC-9] show typing when input text

---
+++ src/utilities/misc.ts
@@ -0,0 +1,8 @@
+export const isOtherUserTyping = (
+  typingData: { [key: string]: boolean },
+  currentUserId: string
+): boolean => {
+  return Object.keys(typingData)
+    .filter((userId) => userId !== currentUserId)
+    .some((userId) => typingData[userId] === true);
+};
